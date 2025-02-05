eba76595e6129a0de91d52f0f47fba79281b44e2
William A. Martinez Martinez <william_martinez@unitec.edu>
2025-02-05 13:44:49 -0600

    Revision

---
+++ src/practica_herencia_semana_/pkg3/Practica_herencia_semana_3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica_herencia_semana_.pkg3;
+
+/**
+ *
+ * @author DELL
+ */
+public class Practica_herencia_semana_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
